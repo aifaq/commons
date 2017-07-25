192c13255e4b2e882ee58eb12e2d935e231d56d0
hzwangwei6 <hzwangwei6@corp.netease.com>
2017-07-25 12:53:59 +0800

    增加数字截取Util

--- commons-lang/src/main/java/me/aifaq/commons/lang/NumberUtil.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/NumberUtil.java
@@ -1,8 +1,38 @@
 package me.aifaq.commons.lang;
 
+import com.google.common.base.Preconditions;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 14:00 2017/6/16
  */
 public class NumberUtil {
+	/**
+	 * 数字截取
+	 *
+	 * @param target
+	 * @param digits 位数，1-个位，2-十位，3-百位，... ...
+	 * @return
+	 */
+	public static int truncate(int target, int digits) {
+		Preconditions.checkArgument(digits > 0, "digits必须大于0");
+
+		return new BigDecimal(target).setScale(-digits, RoundingMode.DOWN).intValue();
+	}
+
+	/**
+	 * 数字截取
+	 *
+	 * @param target
+	 * @param digits 位数，1-个位，2-十位，3-百位，... ...
+	 * @return
+	 */
+	public static long truncate(long target, int digits) {
+		Preconditions.checkArgument(digits > 0, "digits必须大于0");
+
+		return new BigDecimal(target).setScale(-digits, RoundingMode.DOWN).longValue();
+	}
 }
---
+++ commons-lang/src/test/java/me/aifaq/commons/lang/NumberUtilTest.java
@@ -0,0 +1,41 @@
+package me.aifaq.commons.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Wang Wei [5waynewang@gmail.com]
+ * @since 12:32 2017/7/25
+ */
+public class NumberUtilTest {
+	@Test
+	public void testTruncate() {
+		Assert.assertEquals(NumberUtil.truncate(12345, 1), 12340);
+		Assert.assertEquals(NumberUtil.truncate(12345, 2), 12300);
+		Assert.assertEquals(NumberUtil.truncate(12345, 3), 12000);
+		Assert.assertEquals(NumberUtil.truncate(12345, 4), 10000);
+		Assert.assertEquals(NumberUtil.truncate(12345, 5), 0);
+		Assert.assertEquals(NumberUtil.truncate(12345, 6), 0);
+
+		Assert.assertEquals(NumberUtil.truncate(-12345, 1), -12340);
+		Assert.assertEquals(NumberUtil.truncate(-12345, 2), -12300);
+		Assert.assertEquals(NumberUtil.truncate(-12345, 3), -12000);
+		Assert.assertEquals(NumberUtil.truncate(-12345, 4), -10000);
+		Assert.assertEquals(NumberUtil.truncate(-12345, 5), 0);
+		Assert.assertEquals(NumberUtil.truncate(-12345, 6), 0);
+
+		Assert.assertEquals(NumberUtil.truncate(56789, 1), 56780);
+		Assert.assertEquals(NumberUtil.truncate(56789, 2), 56700);
+		Assert.assertEquals(NumberUtil.truncate(56789, 3), 56000);
+		Assert.assertEquals(NumberUtil.truncate(56789, 4), 50000);
+		Assert.assertEquals(NumberUtil.truncate(56789, 5), 0);
+		Assert.assertEquals(NumberUtil.truncate(56789, 6), 0);
+
+		Assert.assertEquals(NumberUtil.truncate(-56789, 1), -56780);
+		Assert.assertEquals(NumberUtil.truncate(-56789, 2), -56700);
+		Assert.assertEquals(NumberUtil.truncate(-56789, 3), -56000);
+		Assert.assertEquals(NumberUtil.truncate(-56789, 4), -50000);
+		Assert.assertEquals(NumberUtil.truncate(-56789, 5), 0);
+		Assert.assertEquals(NumberUtil.truncate(-56789, 6), 0);
+	}
+}
