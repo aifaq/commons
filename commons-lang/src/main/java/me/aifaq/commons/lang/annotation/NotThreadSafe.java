8b580bbeff11dc0fd50056ed04695b00107c426f
wangwei <talk2wangwei@163.com>
2017-11-13 16:18:05 +0800

    OPTIMIZE：support method

--- commons-lang/src/main/java/me/aifaq/commons/lang/annotation/NotThreadSafe.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/annotation/NotThreadSafe.java
@@ -3,11 +3,13 @@ package me.aifaq.commons.lang.annotation;
 import java.lang.annotation.*;
 
 /**
+ * 标明该类或方法不是线程安全的
+ *
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 15:48 2017/5/22
  */
 @Documented
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.CLASS) // The original version used RUNTIME
 public @interface NotThreadSafe {
 }
--- commons-lang/src/main/java/me/aifaq/commons/lang/annotation/ThreadSafe.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/annotation/ThreadSafe.java
@@ -3,11 +3,13 @@ package me.aifaq.commons.lang.annotation;
 import java.lang.annotation.*;
 
 /**
+ * 标明该类或方法是线程安全的
+ *
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 15:45 2017/5/22
  */
 @Documented
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.CLASS) // The original version used RUNTIME
 public @interface ThreadSafe {
 }
