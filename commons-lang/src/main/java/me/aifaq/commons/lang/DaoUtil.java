f792b3940cd2900195928fe509c52e1ddeca2f6d
wangwei <talk2wangwei@163.com>
2018-01-19 14:25:50 +0800

    FEATURE：增加DaoUtil

---
+++ commons-lang/src/main/java/me/aifaq/commons/lang/DaoUtil.java
@@ -0,0 +1,31 @@
+package me.aifaq.commons.lang;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+/**
+ * @author Wang Wei [5waynewang@gmail.com]
+ * @since 14:25 2018/1/19
+ */
+public class DaoUtil {
+    private static final String[] DUPLICATE_KEY_EXCEPTIONS = {
+            "org.springframework.dao.DuplicateKeyException",
+            "com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException"};
+
+    /**
+     * 是否唯一键重复异常
+     *
+     * @param e
+     * @return
+     */
+    public static boolean isDuplicateKeyException(RuntimeException e) {
+        if (ArrayUtils.contains(DUPLICATE_KEY_EXCEPTIONS, e.getClass().getName())) {
+            return true;
+        }
+        final Throwable cause = ExceptionUtils.getRootCause(e);
+        if (cause != null && ArrayUtils.contains(DUPLICATE_KEY_EXCEPTIONS, cause.getClass().getName())) {
+            return true;
+        }
+        return false;
+    }
+}
