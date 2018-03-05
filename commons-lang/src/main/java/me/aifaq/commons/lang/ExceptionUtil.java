a20e3cc1ce7d373c8c24e5111c7811df01af2445
wangwei <talk2wangwei@163.com>
2018-03-05 11:08:40 +0800

    FEATURE：ExceptionUtil 添加获取异常信息

--- commons-lang/src/main/java/me/aifaq/commons/lang/ExceptionUtil.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/ExceptionUtil.java
@@ -1,15 +1,23 @@
 package me.aifaq.commons.lang;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 /**
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 12:34 2017/5/30
  */
 public class ExceptionUtil {
-	public static RuntimeException wrapToRuntimeException(Exception e) {
-		return new RuntimeException(e);
-	}
+    public static RuntimeException wrapToRuntimeException(Exception e) {
+        return new RuntimeException(e);
+    }
 
-	public static RuntimeException wrapToRuntimeException(Exception e, String message) {
-		return new RuntimeException(message, e);
-	}
+    public static RuntimeException wrapToRuntimeException(Exception e, String message) {
+        return new RuntimeException(message, e);
+    }
+
+    public static String getRootCauseStackTrace(Throwable throwable) {
+        final Throwable cause = ExceptionUtils.getRootCause(throwable);
+
+        return ExceptionUtils.getStackTrace(cause == null ? throwable : cause);
+    }
 }
