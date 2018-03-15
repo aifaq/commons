e8a1d1c28963d5380ec06c75894db6dba0f9a398
wangwei <talk2wangwei@163.com>
2018-03-15 10:02:31 +0800

    OPTIMIZE：comment

--- commons-lang/src/main/java/me/aifaq/commons/lang/annotation/Idempotent.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/annotation/Idempotent.java
@@ -3,7 +3,7 @@ package me.aifaq.commons.lang.annotation;
 import java.lang.annotation.*;
 
 /**
- * 标明该操作是幂等的
+ * 标明该方法支持幂等操作
  *
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 16:15 2017/11/13
