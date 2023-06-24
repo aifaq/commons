59029e800e8076e4645c1e6f9dc8c3e14be02678
lucifer <lucifer@89.com>
2023-06-24 11:19:21 +0800

    add

--- commons-lang/src/main/java/me/aifaq/commons/lang/EncodeUtil.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/EncodeUtil.java
@@ -1,8 +1,36 @@
 package me.aifaq.commons.lang;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
 /**
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 16:42 2017/7/7
  */
 public class EncodeUtil {
+    private static final String DEFAULT_URL_ENCODING = "UTF-8";
+
+    /**
+     * UTF-8 编码
+     */
+    public static String urlEncode(String part) {
+        try {
+            return URLEncoder.encode(part, DEFAULT_URL_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            throw ExceptionUtil.wrapToRuntimeException(e);
+        }
+    }
+
+    /**
+     * UTF-8 解码
+     */
+    public static String urlDecode(String part) {
+
+        try {
+            return URLDecoder.decode(part, DEFAULT_URL_ENCODING);
+        } catch (UnsupportedEncodingException e) {
+            throw ExceptionUtil.wrapToRuntimeException(e);
+        }
+    }
 }
--- commons-lang/src/main/java/me/aifaq/commons/lang/ExceptionUtil.java
+++ commons-lang/src/main/java/me/aifaq/commons/lang/ExceptionUtil.java
@@ -2,6 +2,9 @@ package me.aifaq.commons.lang;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * @author Wang Wei [5waynewang@gmail.com]
  * @since 12:34 2017/5/30
@@ -15,9 +18,31 @@ public class ExceptionUtil {
         return new RuntimeException(message, e);
     }
 
-    public static String getRootCauseStackTrace(Throwable throwable) {
-        final Throwable cause = ExceptionUtils.getRootCause(throwable);
+    public static String getRootCauseStackTrace(Throwable t) {
+        final Throwable cause = ExceptionUtils.getRootCause(t);
 
-        return ExceptionUtils.getStackTrace(cause == null ? throwable : cause);
+        return ExceptionUtils.getStackTrace(cause == null ? t : cause);
+    }
+
+    public static String getStackTraceAsString(Throwable t) {
+        StringWriter stringWriter = new StringWriter();
+        t.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
+    }
+
+    /**
+     * 判断异常是否由某些底层的异常引起.
+     */
+    public static boolean isCausedBy(Throwable t, Class<? extends Throwable>... causes) {
+        Throwable cause = t;
+        while (cause != null) {
+            for (Class<? extends Throwable> c : causes) {
+                if (c.isInstance(cause)) {
+                    return true;
+                }
+            }
+            cause = cause.getCause();
+        }
+        return false;
     }
 }
